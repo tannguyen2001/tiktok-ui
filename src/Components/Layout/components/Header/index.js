fac764238be734daa7011b1c4eebaab810b148c7
Tan Nguyen <khodo3136@gmail.com>
2022-12-29 23:10:28 +0700

    build header UI #4

--- src/Components/Layout/components/Header/index.js
+++ src/Components/Layout/components/Header/index.js
@@ -4,14 +4,39 @@ import styles from './Header.module.scss'
 import images from '~/assets/images'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import {
+    faCircleXmark,
+    faSpinner,
+    faMagnifyingGlass,
+    faEllipsisVertical,
+    faEarthAsia,
+    faCircleQuestion,
+    faKeyboard,
+} from '@fortawesome/free-solid-svg-icons'
 import { Wrapper as PopperWrapper } from '../Popper'
 
 import Tippy from '@tippyjs/react/headless'
 import AcountItem from '../../AcountItem'
 import Button from '../Button'
+import Menu from '../Popper/Menu'
 
 const cx = classNames.bind(styles)
+
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and help',
+        to: '/feedback',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcut',
+    },
+]
 
 function Header() {
     const [searchResult, setSearchResult] = useState([])
@@ -56,6 +81,11 @@ function Header() {
                 <div className={cx('actions')}>
                     <Button text>Login</Button>
                     <Button primary>Register</Button>
+                    <Menu items={MENU_ITEMS}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
                 </div>
             </div>
         </header>
